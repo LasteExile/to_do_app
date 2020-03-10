341425685ff7be08f45e53dc49d4a5723da74822
lasteexile <bbriz020903@gmail.com>
2020-03-10 23:02:59 +0200

    Little imporvement

--- todo/urls.py
+++ todo/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('accounts.urls')),
-    path('tasks', include('tasks.urls')),
+    path('tasks/', include('tasks.urls')),
     path('', RedirectView.as_view(url='tasks')),
 ]
