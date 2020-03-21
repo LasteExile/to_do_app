8517d346ed89006ca34f1176516627dd9c68ea1f
lasteexile <bbriz020903@gmail.com>
2020-03-22 00:36:29 +0200

    Finished todo system

--- tasks/urls.py
+++ tasks/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('create', views.CreateTask.as_view(), name='create_page'),
     path('detail/<int:pk>', views.Detail.as_view(), name='detail_page'),
     path('detail/<int:pk>/createsub', views.CreateSubTask.as_view(), name='createsub_page'),
+    path('detail/<int:pk>/edit', views.EditTask.as_view(), name='edittask_page'),
+    path('detail/<int:pk>/editsub/<int:pk1>', views.EditSubTask.as_view(), name='editsubtask_page'),
     path('about', views.About.as_view(), name='about_page'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,9 +1,11 @@
 import datetime
 
 from django.shortcuts import render
-from django.views.generic import ListView, TemplateView, FormView, DetailView
+from django.views.generic import ListView, TemplateView, FormView, DetailView, UpdateView
 from django.db.models.functions import TruncDate
 from django.http.response import HttpResponseRedirect
+from django.urls import reverse
+from django.utils import timezone
 
 from . import models, forms
 
@@ -21,9 +23,9 @@ class Home(ListView):
             models.Task.objects.filter(pk=key).update(completed=True)
         elif type_ == 2:
             models.Task.objects.filter(pk=key).update(completed=False)
-        
+
         return HttpResponseRedirect('/tasks')
-    
+
     def get_context_data(self, **kwards):
         context = super().get_context_data(**kwards)
         if self.request.user.is_authenticated:
@@ -37,22 +39,35 @@ class Detail(DetailView):
     template_name = 'tasks/detail.html'
 
     def post(self, request, pk):
-        params = request.POST.get('taskmarkas').split()
-        type_ = int(params[0])
-        key = int(params[1])
+        if request.POST.get('taskmarkas') != None:
+            params = request.POST.get('taskmarkas').split()
+            type_ = int(params[0])
+            key = int(params[1])
 
-        if type_ == 1:
-            models.Task.objects.filter(pk=key).update(completed=True)
-        elif type_ == 2:
-            models.Task.objects.filter(pk=key).update(completed=False)
-        
+            if type_ == 1:
+                models.Task.objects.filter(pk=key).update(completed=True)
+            elif type_ == 2:
+                models.Task.objects.filter(pk=key).update(completed=False)
+
+        if request.POST.get('sub_taskmarkas') != None:
+            params = request.POST.get('sub_taskmarkas').split()
+            type_ = int(params[0])
+            key = int(params[1])
+
+            if type_ == 1:
+                models.SubTask.objects.filter(task=models.Task.objects.get(
+                    pk=pk)).filter(pk=key).update(completed=True)
+            elif type_ == 2:
+                models.SubTask.objects.filter(task=models.Task.objects.get(
+                    pk=pk)).filter(pk=key).update(completed=False)
+
         return HttpResponseRedirect(f'/tasks/detail/{pk}')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['sub_tasks'] = models.Task.objects.get(pk=self.kwargs['pk']).sub_tasks.all()
+        context['sub_tasks'] = models.Task.objects.get(
+            pk=self.kwargs['pk']).sub_tasks.all()
         return context
-    
 
 
 class CreateTask(FormView):
@@ -62,23 +77,40 @@ class CreateTask(FormView):
 
     def form_valid(self, form):
         time = form.cleaned_data.get('deadline_time')
-        models.Task.objects.create(user=self.request.user, text=self.request.POST.get('text'), deadline_time=time)
+        models.Task.objects.create(
+            user=self.request.user, text=self.request.POST.get('text'), deadline_time=time)
         return HttpResponseRedirect(self.success_url)
 
 
 class CreateSubTask(FormView):
     form_class = forms.CreateNewSubTaskForm
     template_name = 'tasks/createsub.html'
-    success_url = '/tasks'
+    success_url = '/tasks/detail/'
 
     def form_valid(self, form):
-        models.SubTask.objects.create(task=self.request.POST.get('task'), text=self.request.POST.get('text'))
-        return HttpResponseRedirect(self.success_url)
+        models.SubTask.objects.create(task=models.Task.objects.get(
+            pk=self.kwargs['pk']), text=self.request.POST.get('text'))
+        return HttpResponseRedirect(f'self.success_url/{self.kwargs["pk"]}')
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['pk'] = self.kwargs['pk']
-        return context
+
+class EditTask(UpdateView):
+    model = models.Task
+    fields = ['text', 'deadline_time']
+    template_name = 'tasks/task_edit.html'
+
+    def get_success_url(self):
+        return reverse('detail_page', args=[str(self.kwargs['pk'])])
+
+
+class EditSubTask(UpdateView):
+    pk_url_kwarg = 'pk1'
+    model = models.SubTask
+    fields = ['text']
+    template_name = 'tasks/subtask_edit.html'
+
+    def get_success_url(self):
+        return reverse('detail_page', args=[str(self.kwargs['pk'])])
+
 
 class About(TemplateView):
     template_name = 'tasks/about.html'
